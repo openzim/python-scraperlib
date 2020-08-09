2c4d976d50b9069d9eee32bc8b66767f931418e3
Satyam Kumar <io.satyamtg@gmail.com>
2020-08-09 10:39:36 +0530

    Fixes #42 - Add mime mapping to deal with inconsistencies in magic mime detection

--- src/zimscraperlib/filesystem.py
+++ src/zimscraperlib/filesystem.py
@@ -11,12 +11,22 @@ import pathlib
 
 import magic
 
+# a mapping of various forms of the same mimetype to point to a single one
+# this is necessary to deal with inconsistencies in the magic database on different platforms
+MIME_MAP = {
+    "image/svg": "image/svg+xml",
+}
+
 
 def get_file_mimetype(fpath: pathlib.Path) -> str:
     """ MIME Type of file retrieved from magic headers """
-    return magic.detect_from_filename(fpath).mime_type
+
+    detected_mime = magic.detect_from_filename(fpath).mime_type
+    return MIME_MAP.get(detected_mime, detected_mime)
 
 
 def get_content_mimetype(content: bytes) -> str:
     """ MIME Type of content retrieved from magic headers """
-    return magic.detect_from_content(content).mime_type
+
+    detected_mime = magic.detect_from_content(content).mime_type
+    return MIME_MAP.get(detected_mime, detected_mime)
