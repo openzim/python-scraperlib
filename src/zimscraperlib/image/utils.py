87c0f33b42bb247ed986a2bf3f418a6d2fa318e3
benoit74 <benoit74@users.noreply.github.com>
2024-02-12 21:42:18 +0100

    Add more admissible types for convert_image + corresponding tests

--- src/zimscraperlib/image/utils.py
+++ src/zimscraperlib/image/utils.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+import io
 import pathlib
-from typing import Optional
+from typing import Optional, Union
 
 from PIL import Image
 
 
 def save_image(
     src: Image,  # pyright: ignore
-    dst: pathlib.Path,
+    dst: Union[pathlib.Path, io.BytesIO],
     fmt: Optional[str] = None,
-    **params: Optional[dict],
+    **params: str,
 ) -> None:
     """PIL.Image.save() wrapper setting default parameters"""
     args = {"JPEG": {"quality": 100}, "PNG": {}}.get(fmt, {})  # pyright: ignore
