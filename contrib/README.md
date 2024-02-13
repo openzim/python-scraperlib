2fc4061ba5ff7f46dc5fd70f04bb720d23694930
benoit74 <benoit74@users.noreply.github.com>
2024-02-13 14:33:14 +0100

    Add a small utility to encode a video as scraper would do

---
+++ contrib/README.md
@@ -0,0 +1,3 @@
+
+This folder contains some tooling around zimscraperlib:
+- `encode_video.py`: a small utility to encode a video with an existing video preset, just like a scraper would do
---
+++ contrib/encode_video.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+from typing import List
+
+from zimscraperlib import logger
+from zimscraperlib.video import presets, reencode
+
+
+def encode_video(src_path: Path, dst_path: Path, preset: str):
+    if not src_path.exists():
+        raise ValueError(f"{src_path} does not exists")
+    try:
+        preset_cls = getattr(presets, preset)
+    except AttributeError:
+        logger.error(f"{preset} preset not found")
+        raise
+    logger.info(f"Encoding video {src_path} with {preset} version {preset_cls.VERSION}")
+    success, process = reencode(
+        src_path=src_path,
+        dst_path=dst_path,
+        ffmpeg_args=preset_cls().to_ffmpeg_args(),
+        with_process=True,
+    )  # pyright: ignore[reportGeneralTypeIssues] (returned type is variable, depending on `with_process` value)
+    if not success:
+        logger.error(f"conversion failed:\n{process.stdout}")
+
+
+def run(args: List[str] = sys.argv):
+    if len(args) < 4:  # noqa: PLR2004
+        print(f"Usage: {args[0]} <src_path> <dst_path> <preset>")  # noqa: T201
+        print(  # noqa: T201
+            "\t<src_path>\tpath to the video to encode."
+            "\t<dst_path>\tpath to the store the reencoded video."
+            "\t<preset>\tname of preset to use."
+        )
+        return 1
+    encode_video(Path(args[1]), Path(args[2]), args[3])
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(run())
